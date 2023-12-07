b1ba64a22082f05c9c2f001f5c058f0b732466bc
nutankumari211 <nutankumari211@gmail.com>
2023-12-07 15:40:27 +0530

    main

---
+++ frontend/src/components/UserList.js
@@ -0,0 +1,60 @@
+// UserList.js
+import React, { useState } from 'react';
+import UserEdit from './UserEdit';
+
+const UserList = ({ users, onDelete, onEdit }) => {
+  const [editUserId, setEditUserId] = useState(null);
+
+  const handleEdit = (user) => {
+    setEditUserId(user.id);
+  };
+
+  const handleUserUpdated = () => {
+    setEditUserId(null);
+  };
+
+  return (
+    <div className="user-list-container">
+      <h2>User List</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Password</th>
+            <th>Roles</th>
+            <th>Position</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.username}</td>
+              <td className="password-cell">*****</td> {/* Display masked password */}
+              <td>{user.roles.join(', ')}</td>
+              <td>{user.position}</td>
+              <td>
+                <button className="edit-button" onClick={() => handleEdit(user)}>
+                  Edit
+                </button>{' '}
+                <button className="delete-button" onClick={() => onDelete(user.id)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {editUserId && (
+        <UserEdit
+          userId={editUserId}
+          onClose={() => setEditUserId(null)}
+          onUserUpdated={handleUserUpdated}
+        />
+      )}
+    </div>
+  );
+};
+
+export default UserList;
